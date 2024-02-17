1fda424b138d804d56e2759862ee563db7a8fa39
mehrabkb <mkordbacheh75@gmail.com>
2024-02-17 14:27:42 +0330

    add the price list menu and try to send pdf files in telegram bot

--- bot.php
+++ bot.php
@@ -3,15 +3,18 @@
     $inData = file_get_contents("php://input");
     $tData = json_decode($inData);
     reply($tData);
-
     function reply($data){
         $textMessage = $data->message->text;
         switch ($textMessage){
+            case 'خانه':
             case '/start' :
                 sender($data->message->chat->id , welcomeMessage());
                 break;
             case 'نرخ دلار':
                 dollorPriceSender($data->message->chat->id);
+                break;
+            case 'لیست قیمت':
+                listPriceMenu($data->message->chat->id , 'یکی از موارد زیر را انتخاب کنید');
                 break;
             default :
                 sender($data->message->chat->id , welcomeMessage());
@@ -21,7 +24,7 @@
 
     function sender($chat_id , $text){
         $main_keyboard = [
-            ['لیست قیمت ' , 'نرخ دلار']
+            ['لیست قیمت' , 'نرخ دلار']
         ];
         bot('sendMessage' , [
             'chat_id' => $chat_id,
@@ -31,7 +34,7 @@
     }
     function dollorPriceSender($chat_id ){
         $main_keyboard = [
-            ['لیست قیمت ' , 'نرخ دلار']
+            ['لیست قیمت' , 'نرخ دلار']
         ];
         include ('dollorprice.php');
         $text = $finalText;
@@ -42,6 +45,19 @@
             'reply_markup' => json_encode(['keyboard' => $main_keyboard , 'resize_keyboard' => true])
         ]);
     }
+    function listPriceMenu($chat_id , $text = ''){
+        $list_price_keyboard = [
+            ['میراب'],
+            ['بنکن'],
+            ['پلیران'],
+            ['خانه']
+        ];
+        bot('sendMessage' , [
+            'chat_id' => $chat_id,
+            'text' => $text,
+            'reply_markup' => json_encode(['keyboard' => $list_price_keyboard , 'resize_keyboard' => true])
+        ]);
+    }
 
 
 function bot($method , $data=[]){
@@ -50,6 +66,7 @@ function bot($method , $data=[]){
         curl_setopt($ch , CURLOPT_URL , $url);
         curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
         curl_setopt($ch , CURLOPT_POSTFIELDS , $data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data'));
         $result = curl_exec($ch);
 
         return $result;
